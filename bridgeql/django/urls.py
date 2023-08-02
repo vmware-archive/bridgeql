3bf11bf80a5e9ccd0bb1351060228732d31f994f
Priyank Singh <priyanksi@vmware.com>
2023-08-02 16:06:12 +0530

    Patch 0.2.2 (#37)
    * urls: remove python2 support for url
    
    * url: use url function for py2 and re_path for py3
    
    Signed-off-by: Priyank Singh <priyanksi@vmware.com>

--- bridgeql/django/urls.py
+++ bridgeql/django/urls.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2023 VMware, Inc.  All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
-
 try:
-    from django.urls import path
+    from django.urls import re_path as url
 except ImportError:
-    from django.conf.urls import url as path
+    from django.conf.urls import url
 
 from bridgeql.django import bridge
 from bridgeql.django.settings import bridgeql_settings
@@ -14,16 +13,16 @@ from bridgeql.django.views import index, generate_bridgeql_schema
 bridgeql_settings.validate()
 
 urlpatterns = [
-    path('create/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/',
+    url(r'^create/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/$',
          bridge.create_django_model, name='bridgeql_django_create'),
-    path('read/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/(?P<pk>\w+)/',
+    url(r'^read/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/(?P<pk>\w+)/$',
          bridge.read_django_model, name='bridgeql_django_read_pk'),
-    path('read/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/',
+    url(r'^read/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/$',
          bridge.read_django_model, name='bridgeql_django_read'),
-    path('update/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/(?P<pk>\w+)/',
+    url(r'^update/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/(?P<pk>\w+)/$',
          bridge.update_django_model, name='bridgeql_django_update'),
-    path('delete/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/(?P<pk>\w+)/',
+    url(r'^delete/(?P<db_name>\w+)/(?P<app_label>\w+)/(?P<model_name>\w+)/(?P<pk>\w+)/$',
          bridge.delete_django_model, name='bridgeql_django_delete'),
-    path('schema/', generate_bridgeql_schema, name='generate_bridgeql_schema'),
-    path('', index, name='bridgeql_django_index'),
+    url(r'^schema/$', generate_bridgeql_schema, name='generate_bridgeql_schema'),
+    url(r'^/', index, name='bridgeql_django_index'),
 ]
